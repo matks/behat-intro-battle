73d7e0d84222cde6ef1be83eb275bd12d820aadf
Mathieu Ferment <mathieu.ferment@prestashop.com>
2019-01-03 17:29:43 +0100

    Add missing bootstrap file

---
+++ bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+// bootstrap.php
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+require_once "vendor/autoload.php";
+
+$isDevMode = true;
+$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/src"), $isDevMode);
+
+// database configuration parameters
+$conn = array(
+    'driver' => 'pdo_sqlite',
+    'path' => __DIR__ . '/db.sqlite',
+);
+
+// obtaining the entity manager
+return $entityManager = EntityManager::create($conn, $config);
